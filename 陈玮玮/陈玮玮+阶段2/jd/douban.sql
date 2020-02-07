db1972b28472ea18923bda62571b4f0eae435ceb
cww <wei013570@163.com>
2020-02-07 14:44:15 +0800

    stage_2_sql

---
+++ 陈玮玮/陈玮玮+阶段2/jd/douban.sql
@@ -0,0 +1,23 @@
+﻿SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `sex` varchar(8) DEFAULT NULL,
+  `age` varchar(100) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `circle_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9353 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'admin', '123321', null, '50', null, null, '1');
+INSERT INTO `user` VALUES ('9350', 'test1', '123456', '男', '22', '123@123.com', '123321123321', null);
